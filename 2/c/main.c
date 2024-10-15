e0267f68af0b46a0134094f1ff3c16aafa05cbb9
Christian Bouwense <cbouwense@proton.me>
2024-10-15 14:03:39 -0400

    solved it with only one allocation

--- 2/c/main.c
+++ 2/c/main.c
@@ -20,58 +20,73 @@ int main(void) {
         GupArrayString lines = gup_file_read_lines_arena(&a, "../input.txt");
     
         for (int i = 0; i < lines.count; i++) {
-            // ["Game 1738", " 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue"]
-            GupArrayString tokens = gup_string_split_arena(&a, lines.data[i], ':');
-
-            // [" 1 blue, 2 green", " 3 green, 4 blue, 1 red", " 1 green, 1 blue"]
-            GupArrayString rounds = gup_string_split_arena(&a, tokens.data[1], ';');
-            // ["1 blue, 2 green", "3 green, 4 blue, 1 red", "1 green, 1 blue"]
-            gup_array_string_map_in_place(&rounds, trim_whitespace);
+            // "Game 1: 7 green, 4 blue, 3 red; 4 blue, 10 red, 1 green; 1 blue, 9 red"
+            GupString line = lines.data[i];
 
             int max_red = 0, max_green = 0, max_blue = 0;
-            for (int j = 0; j < rounds.count; j++) {
-                // ["1 blue", " 2 green"]
-                GupArrayString draws = gup_string_split_arena(&a, rounds.data[j], ',');
-                // ["1 blue", "2 green"]
-                gup_array_string_map_in_place(&draws, trim_whitespace);
-                for (int k = 0; k < draws.count; k++) {
-                    // ["1", "blue"]
-                    GupArrayString draw_tokens = gup_string_split_arena(&a, draws.data[k], ' ');
+
+            bool passed_colon = false;
+            bool parsing_count = false;
+            for (int j = 0; j < line.count; j++) {
+                if (!passed_colon) {
+                    if (line.data[j] != ':') continue;
+
+                    passed_colon = true;
+                }
+
+                if (!parsing_count) {
+                    if (!is_digit(line.data[j])) continue;
+
+                    parsing_count = true;
+                }
+
+                if (parsing_count) {
+                    int digit_len = 0;
+                    while (is_digit(line.data[j])) {
+                        j++;
+                        digit_len++;
+                    }
+                    // Skip past the space between the count and the number (e.g. "1337 blue")
+                    j++;
+
+                    GupString count_view = (GupString){
+                        .count = digit_len,
+                        .capacity = digit_len,
+                        .data = &line.data[j - digit_len - 1],
+                    };
+
                     int count;
-                    gup_assert_verbose(gup_string_to_int(draw_tokens.data[0], &count), "Expected draw to have a count but it didn't");
-                    if (gup_string_eq(draw_tokens.data[1], gup_string(&a, "red"))) {
+                    gup_assert(gup_string_to_int(count_view, &count));
+                    if (line.data[j] == 'r') {
+                        j += 2;
                         if (count > max_red) {
                             max_red = count;
                         }
                     }
-                    if (gup_string_eq(draw_tokens.data[1], gup_string(&a, "green"))) {
+                    if (line.data[j] == 'g') {
+                        j += 4;
                         if (count > max_green) {
                             max_green = count;
                         }
                     }
-                    if (gup_string_eq(draw_tokens.data[1], gup_string(&a, "blue"))) {
+                    if (line.data[j] == 'b') {
+                        j += 3;
                         if (count > max_blue) {
                             max_blue = count;
                         }
                     }
+
+                    parsing_count = false;
                 }
             }
 
             bool game_is_impossible = max_red <= max_possible_red && max_green <= max_possible_green && max_blue <= max_possible_blue;
             if (game_is_impossible) {
-                // "Game 1738" -> "1738"
-                GupString game_id_str = gup_string_filter_arena(&a, tokens.data[0], is_digit);
-
-                // "1738" -> 1738
-                int game_id;
-                gup_assert(gup_string_to_int(game_id_str, &game_id));
-
-                part_one_answer += game_id;
+                part_one_answer += i + 1;
             }
         }
 
         printf("answer: %d\n", part_one_answer);
-        gup_assert(part_one_answer == 8);
     }
     
     gup_arena_destroy(a);
